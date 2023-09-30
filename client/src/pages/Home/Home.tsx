8a284fb8e8a7923f985ca7d1e77308e362cc8eed
Jacob <103798568+j-tad@users.noreply.github.com>
2023-09-30 16:29:34 +0000

    Add Hello World

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -19,6 +19,7 @@ function Home() {
   return (
     <Page>
       <div className="home-page">
+        <p>Hello World</p>
         <h1 className="home-page__title">Home</h1>
         <h2>Products:</h2>
         <div className="home-page__products">
