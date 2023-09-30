161d3f2d7d31e48d3168359675d9251fe1e4a88d
Joey Guan <78620583+JoeyGuan@users.noreply.github.com>
2023-09-30 16:48:54 -0400

    #16 Did Login

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -5,13 +5,16 @@ import { useAccountContext } from "../../context";
 import "./Login.style.scss";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const [message, setMessage] = useState(null);
   const { loggedIn, login } = useAccountContext();
   const navigate = useNavigate();
 
   const attemptLogin = async () => {
     try {
-      const message = await login("admin@email.com", "password");
+      const message = await login(email, password);
       setMessage(message);
     } catch (error) {
       console.log(error);
@@ -28,10 +31,26 @@ function Login() {
     <Page>
       <div className="login-page">
         <h1>Login</h1>
-        <button onClick={() => attemptLogin()}>
-          Login (as user set in code)
-        </button>
-        {message && <p>{message}</p>}
+
+      <form>
+        <div>
+          <input type = "text" value = {email} placeholder = "email" 
+          onChange={(e) => setEmail(e.target.value)}></input>
+        </div>
+
+        <div>
+          <input type = "text" value = {password} placeholder = "password" 
+          onChange={(e) => setPassword(e.target.value)}></input>
+        </div>
+      </form>
+
+        <div>
+          <button onClick={() => attemptLogin()}>
+            Login (as user set in code)
+          </button>
+          {message && <p>{message}</p>}
+        </div>
+
       </div>
     </Page>
   );
